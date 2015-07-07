b77f221b91aea474c4bf2058bc69d811d3c79d56
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-07-07 17:45:28 +0300

    Refs #26601; Integrating AIDE app with new eea.searchserver.js

--- app/public/javascripts/aide.facetview.js
+++ app/public/javascripts/aide.facetview.js
@@ -70,24 +70,30 @@ jQuery(document).ready(function($) {
         initial_search: false,
         enable_rangeselect: true,
         enable_geoselect: true,
-        post_search_callback: viewReady,
-        pushstate : false,
         default_sort: default_sort,
         rangefacets: [field_base + 'airqualityValue_aqvalue', field_base + 'datacapturePct_datacapture'],
+        post_init_callback: function() {
+          add_EEA_settings();
+        },
+        post_search_callback: function() {
+          add_EEA_settings();
+          viewReady();
+        },
+
         facets: [
 //            {'field':'_id', 'display': '_id', 'order': 'term'},
-            {'field':field_base + 'inspireNamespace', 'display': 'Namespace', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'beginPosition_year', 'display': 'Year', 'size':'50', 'order': 'term'}, //?
-            {'field':field_base + 'pollutant', 'display': 'Pollutant', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'aggregationType', 'display': 'AggregationType', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'airqualityValue_aqvalue', 'display': 'AQvalue', 'size':'10000000', 'order': 'term'},
-            {'field':field_base + 'datacapturePct_datacapture', 'display': 'DataCapture', 'size':'10000000', 'order': 'term'},
-            {'field':field_base + 'samplingPoint_stationtype', 'display': 'StationType', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'station_stationarea', 'display': 'StationArea', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'procedure_analyticaltechnique', 'display': 'AnalyticalTechnique', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'samplingPoint_zone', 'display': 'Zone', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'samplingPoint_zoneCountryLabel', 'display': 'ZoneCountryLabel', 'size':'50', 'order': 'term'},
-            {'field':field_base + 'station_geo_pos', 'display': 'Geo location', 'size':'2', 'order': 'term'}
+            {'field':field_base + 'inspireNamespace', 'display': 'Namespace', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'beginPosition_year', 'display': 'Year', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']}, //?
+            {'field':field_base + 'pollutant', 'display': 'Pollutant', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'aggregationType', 'display': 'AggregationType', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'airqualityValue_aqvalue', 'display': 'AQvalue', 'size':'10000000', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+//            {'field':field_base + 'datacapturePct_datacapture', 'display': 'DataCapture', 'size':'10000000', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'samplingPoint_stationtype', 'display': 'StationType', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'station_stationarea', 'display': 'StationArea', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'procedure_analyticaltechnique', 'display': 'AnalyticalTechnique', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'samplingPoint_zone', 'display': 'Zone', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+            {'field':field_base + 'samplingPoint_zoneCountryLabel', 'display': 'ZoneCountryLabel', 'size':'50', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']},
+//            {'field':field_base + 'station_geo_pos', 'display': 'Geo location', 'size':'2', 'order': 'term', 'facet_display_options': ['sort', 'checkbox']}
         ],
 
 
