95a2f97bdbe8e5ee73d2d87651bba8486233a6a5
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-08-03 17:55:05 +0300

    Refs #25509; Updated details page, so it can be configured from a configuration file

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,7 +13,8 @@ var searchServer = require('eea-searchserver')
 
 var fieldsMapping = [];
 var mapping = require("../mapping.json");
-var fieldsMapping = mapping.details_mapping;
+var fieldsMapping = mapping.fields_mapping;
+var details_settings = mapping.details_settings;
 
 exports.index = function(req, res){
   var options = {title: 'aide'};
@@ -21,7 +22,11 @@ exports.index = function(req, res){
   searchServer.EEAFacetFramework.render(req, res, 'index', options);
 };
 
+
 exports.details = function(req, res){
+  function sort_elements(a, b) {
+    return a.pos - b.pos
+  }
 
   if (req.query.aideid === undefined){
       res.send('aideid is missing');
@@ -40,6 +45,22 @@ exports.details = function(req, res){
     path: "?source="+ query
   };
 
+  sections = details_settings.sections;
+  sections.sort(sort_elements);
+  var details_fields = {};
+  for (var idx = 0; idx < fieldsMapping.length; idx++) {
+    if (fieldsMapping[idx].details.visible){
+      if (details_fields[fieldsMapping[idx].details.section] === undefined){
+        details_fields[fieldsMapping[idx].details.section] = []
+      }
+      fieldsMapping[idx].details.name = fieldsMapping[idx].name;
+      details_fields[fieldsMapping[idx].details.section].push(fieldsMapping[idx].details)
+    }
+  }
+  for (var idx = 0; idx < sections.length; idx++){
+    details_fields[sections[idx].name].sort(sort_elements);
+    sections[idx].fields = details_fields[sections[idx].name];
+  }
   request(options.host + options.path, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         try{
@@ -54,16 +75,20 @@ exports.details = function(req, res){
             resultobj = {};
             var value;
             for (var idx = 0; idx < fieldsMapping.length; idx++) {
-                value = tmp_resultobj["records"][0][field_base + fieldsMapping[idx]['field']];
-                label = fieldsMapping[idx]['title'];
+                value = tmp_resultobj["records"][0][field_base + fieldsMapping[idx]['name']];
+                label = fieldsMapping[idx]['details']['title'];
+
                 if (label.substr(label.length - 5,5) === "_link"){
                     value = encodeURIComponent(value);
                 }
+
                 resultobj[fieldsMapping[idx]['name']] = {'label':label, 'value':value};
             }
-            resultobj['_shorttitle'].value = resultobj['_id'].value.split('/')[resultobj['_id'].value.split('/').length - 1];
+//            resultobj['_shorttitle'].value = resultobj['_id'].value.split('/')[resultobj['_id'].value.split('/').length - 1];
+            resultobj['_shorttitle'] = {label:'_shorttitle', value : resultobj['_id'].value}
             var options = {data: resultobj,
-                        field_base: field_base};
+                        field_base: field_base,
+                        sections: sections};
             searchServer.EEAFacetFramework.render(req, res, 'details', options);
 
         }
