2d4f77b749fd7343069cbcbdcb6affd9f3d4e83f
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-09-02 17:55:19 +0300

    Refs #28454; Refractored the indexing from query with filters, moved to eea.searchserver.js npm package

--- app/management/commands.js
+++ app/management/commands.js
@@ -40,87 +40,11 @@ function removeData() {
         .execute();
 }
 
-function fetchQuery(query, fetchcallback) {
-    var batch_head = '{"index":{}}'
-
-    var elastic = require('nconf').get('elastic');
-    var SparqlClient = require('sparql-client');
-    var client = new SparqlClient(config.endpoint);
-
-    client.query(query).execute(function(error, results){
-        var rows_str = "";
-        for (var i = 0; i < results.results.bindings.length; i++){
-            var toindex = {};
-            for (var j = 0; j < results.head.vars.length; j++){
-                if (results.results.bindings[i][results.head.vars[j]] !== undefined){
-                    toindex[results.head.vars[j]] = results.results.bindings[i][results.head.vars[j]].value;
-                }
-            }
-            rows_str += batch_head.split("{}").join('{"_id":"' + results.results.bindings[i]["_id"].value + '"}');
-            rows_str += "\n";
-            rows_str += JSON.stringify(toindex);
-            rows_str += "\n";
-        }
-        new esAPI(getOptions())
-            .POST(elastic.index+"/"+elastic.type+"/_bulk", rows_str, callback("indexed " + results.results.bindings.length + " rows", false))
-            .execute();
-        fetchcallback(results.results.bindings.length);
-    });
-}
-
-var bigTotal = 0;
-function fetchQueries(queries, idx){
-    if (idx === undefined){
-        idx = 0;
-    }
-
-    if (idx === queries.length){
-        return;
-    }
-    console.log("Fetching query #", idx + 1, " of ", queries.length);
-    console.log("Filters:");
-    console.log(queries[idx].filters);
-    fetchQuery(queries[idx].query, function(indexed){
-        bigTotal += indexed;
-        console.log("Total indexed:", bigTotal);
-        fetchQueries(queries, idx + 1)
-    });
-}
-
-function createQueries(base_query, filters_query, callback){
-    var queries = [];
-    var elastic = require('nconf').get('elastic');
-    var SparqlClient = require('sparql-client');
-    var client = new SparqlClient(config.endpoint);
-    client.query(filters_query).execute(function(error, results){
-        for (var i = 0; i < results.results.bindings.length; i++){
-            var tmp_query = {query:base_query, filters:{}};
-            for (var j = 0; j < results.head.vars.length; j++){
-                var attr = results.head.vars[j];
-                var value = results.results.bindings[i][results.head.vars[j]].value;
-                tmp_query.query = tmp_query.query.split("<" + attr + ">").join(value);
-                tmp_query.filters[attr] = value;
-            }
-            queries.push(tmp_query);
-        }
-        callback(queries);
-    });
-}
 
 function createIndex() {
     var elastic = require('nconf').get('elastic');
-    var SparqlClient = require('sparql-client');
-    var client = new SparqlClient(config.endpoint);
-    var elastic = require('nconf').get('elastic');
-    createQueries(config.queryTemplate, config.filtersQuery, function(queries){
-        new esAPI(getOptions())
-            .DELETE(elastic.index, callback('Deleting index (if it exists)'))
-            .PUT(elastic.index, analyzers, 
-                    function(){
-                        fetchQueries(queries);
-                    })
-            .execute();
-    });
+    new esAPI(getOptions())
+            .indexFromQuery(config.endpoint, config.queryTemplate, config.filtersQuery, elastic, analyzers);
 }
 
 function showHelp() {
